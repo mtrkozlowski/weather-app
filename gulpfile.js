8eb78f92cdc4863ed92b8c1617ed5bc80d71f9b5
mtrkozlowski <mtrkozlowski@gmail.com>
2020-01-18 12:42:01 +0100

    First steps with the project

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const notifier = require('node-notifier');
+
+sass.compiler = require('node-sass');
+
+function mySassError(err) {
+    console.log(err.messageFormatted);
+    notifier.notify({
+        title: 'Blad kompilacji SASS',
+        message: err.messageFormatted
+    });
+}
+
+function server(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        browser: "chrome"
+    });
+
+    cb();
+}
+
+
+function css() {
+    return gulp.src('./scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "expanded" // nested, expanded, compact, compressed
+        }).on('error', mySassError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
+}
+
+function watch() {
+    gulp.watch('./scss/**/*.scss', gulp.series(css));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}
+
+
+exports.css = css;
+exports.default = gulp.series(css,server, watch);
